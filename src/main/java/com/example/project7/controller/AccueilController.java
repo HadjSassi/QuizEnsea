56b5983020380468a53395f374b5f589244d34cc
HadjSassi <mahdihadjsassi1@gmail.com>
2024-11-05 15:26:46 +0100

    Vider Acceuil.fxml Ajouter les fichires nécéssaires pour les nouveaux interfaces.

--- src/main/java/com/example/project7/controller/AccueilController.java
+++ src/main/java/com/example/project7/controller/AccueilController.java
@@ -17,49 +17,12 @@ import java.util.ResourceBundle;
 
 
 public class AccueilController implements Initializable {
-    @FXML
-    private Pagination p;
 
-    private List<String> list = new ArrayList<String>();
+
 
     //ImageView imageView;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-//            list.add("/com/example/project7/images/1.jpg");
-            list.add("images/1.jpg");
-            list.add("images/2.jpg");
-            list.add("images/3.jpg");
-            list.add("images/4.jpg");
-            list.add("images/5.jpg");
-            list.add("images/6.jpg");
-            list.add("images/7.jpg");
-
-
-        Image images[] = new Image[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            try {
-                images[i] = new Image( getClass().getResource("/com/example/project7/"+list.get(i)).toURI().toString());
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        p.setPageFactory((Integer pageIndex) -> {
-            ImageView im = new ImageView(images[pageIndex]);
-            im.setFitHeight(515);
-            im.setFitWidth(793);
-            return im;
-        });
-
-        //imageview.setCursor(Cursor.CLOSED_HAND);
-        Timeline fiveSecondsWonder = new Timeline(new KeyFrame(Duration.seconds(5), event -> {
-            if (p.getCurrentPageIndex() < list.size() - 1) {
-                int pos = (p.getCurrentPageIndex() + 1) % p.getPageCount();
-                p.setCurrentPageIndex(pos);
-            } else
-                p.setCurrentPageIndex(0);
-        }));
-        fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
-        fiveSecondsWonder.play();
     }
 }
