40c08761a6bcba445ff5f55062328299c5649450
HadjSassi <mahdihadjsassi1@gmail.com>
2024-11-12 17:34:57 +0100

    Add icons, correct the border and bars !

--- src/main/java/com/example/project7/controller/edition/EditerSection.java
+++ src/main/java/com/example/project7/controller/edition/EditerSection.java
@@ -1,4 +1,46 @@
 package com.example.project7.controller.edition;
 
-public class EditerSection {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditerSection implements Initializable {
+
+    @FXML
+    private TextField identifiantSection;
+
+    @FXML
+    private SplitMenuButton typeSection;
+
+    @FXML
+    private Button ajouterSection;
+
+    @FXML
+    private Button cancelSection;
+
+    @FXML
+    private AnchorPane sectionPane;
+
+    @FXML
+    public void handleClicksAddSection(ActionEvent event) {
+        //todo save in the database and return the previous interface which is the Editer Projet.
+    }
+
+    @FXML
+    public void handleClicksCancel(ActionEvent event) {
+        //todo just return the previous interface which is the Editer Projet.
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/com/example/project7/controller/edition/SelectionTypeProjet.java
+++ src/main/java/com/example/project7/controller/edition/SelectionTypeProjet.java
@@ -1,4 +1,46 @@
 package com.example.project7.controller.edition;
 
-public class SelectionTypeProjet {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SelectionTypeProjet implements Initializable {
+
+    @FXML
+    private Button create;
+
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField location;
+
+    @FXML
+    private MenuButton typeProject;
+
+    @FXML
+    public void handleClicksCreate(ActionEvent event) {
+        //todo create a new project that will be inserted in the database, and switch to the next interface editer projet
+    }
+
+
+    @FXML
+    public void handleClicksCancel(ActionEvent event) {
+        //todo just return the previous interface which is the acceuil.
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
